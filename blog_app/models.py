82507554f81af85b5079cc1f42624d972e0dee24
Petr Ushakov <p.ushakov@yadro.com>
2021-05-20 22:50:34 +0300

    Create common blog models and migrations

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class BlogPost(models.Model):
+    STATUS_PUBLISHED, STATUS_DRAFT = [str(num) for num in range(2)]
+
+    STATUS_CHOICES = [
+        (STATUS_PUBLISHED, 'Published'),
+        (STATUS_DRAFT, 'Draft'),
+    ]
+
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=4096)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_DRAFT)
+
+
+class BlogPostComment(models.Model):
+    content = models.TextField(max_length=4096)
+    create_time = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
